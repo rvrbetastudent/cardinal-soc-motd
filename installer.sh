fa475cbf584ce6bce6b8b65ec6c76e88635b88d8
rvrbetastudent <rsvrbeta@students.nic.edu>
2026-03-23 13:32:43 -0700

    Add installer script for cardinal-soc-motd

---
+++ installer.sh
@@ -0,0 +1,3 @@
+wget -P /opt/ https://github.com/rvrbetastudent/cardinal-soc-motd/archive/refs/heads/main.zip 
+unzip /opt/main.zip
+mv /opt/cardinal-soc-motd/cardinal-soc.sh /etc/profile.d/cardinal-soc.sh
